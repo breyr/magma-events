18deba45d83a61acce8df4941c7ef250bf2ce70d
breyr <breyr2021@gmail.com>
2024-03-18 14:02:50 -0400

    initial config

--- src/includes/connect.php
+++ src/includes/connect.php
@@ -1,5 +1,9 @@
 <?php
-    $conn = new mysqli("<database service name>", "<user>", "<password>", "<database>");
+    $DB_SERVICE = $_ENV['DB_SERVICE'];
+    $DB_USER = $_ENV['MYSQL_USER'];
+    $DB_PASS = $_ENV['MYSQL_PASSWORD'];
+    $DB_NAME = $_ENV['MYSQL_DATABASE'];
+    $conn = new mysqli($DB_SERVICE, $DB_USER, $DB_PASS, $DB_NAME);
     if(mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
